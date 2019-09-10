38dc31299cc34f7f29b56db42cb7dd687e61c10b
Martin Disch <martindisch@gmail.com>
2019-09-10 17:46:18 +0200

    Implement parsing arrays of integers

--- src/lib.rs
+++ src/lib.rs
@@ -22,14 +22,28 @@ pub fn run(input: &str, coap: bool) -> Result<()> {
 
 /// Converts the input (hexstring or byte array notation) to bytes.
 fn to_bytes(s: &str) -> Result<Vec<u8>> {
-    // TODO: Implement byte array notation
     // Clone the input so we can manipulate it
     let mut input = s.to_string();
     // Remove any whitespace
     input.retain(|c| !c.is_whitespace());
 
+    let bytes = if input.find("[").is_some() {
+        // This appears to be formatted as a byte array, so remove the opening
+        // and closing brackets
+        let input: String = input.drain(1..input.len() - 1).collect();
+        // Split at commas and try to parse values as integers
+        input
+            .split(",")
+            .map(|n| n.parse::<u8>())
+            .map(|v| v.map_err(|e| e.into()))
+            .collect::<Result<Vec<u8>>>()?
+    } else {
+        // We have normal hex input
+        hex::decode(&input)?
+    };
+
     // Attempt conversion
-    Ok(hex::decode(&input)?)
+    Ok(bytes)
 }
 
 /// Returns the bytes formatted as a byte array in hexadecimal notation with
